08d1928ae1e4d3c4d267b24c5a203e8de1964607
Andrew Barchuk <andrew@raindev.io>
2023-05-14 22:32:03 +0200

    Set background to match system appearance on macOS
    Based on the system appearance during start-up, without reacting to the
    changes

--- lua/macos.lua
+++ lua/macos.lua
@@ -1,7 +1,14 @@
 local M = {}
 
 function M.setup()
-  -- TODO
+  local defaults_cmd = 'defaults read -g AppleInterfaceStyle'
+  -- the command returns 'Dark' for dark mode and fails otherwise with
+  -- The domain/default pair of (kCFPreferencesAnyApplication, AppleInterfaceStyle) does not exist
+  if os.execute(defaults_cmd) == 0 then
+    vim.opt.background = 'dark'
+  else
+    vim.opt.background = 'light'
+  end
 end
 
 return M
